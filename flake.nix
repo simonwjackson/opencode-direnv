a002d7adad62b619ae1d4accdd036c17983b79bb
Simon W. Jackson <github@simonwjackson.io>
2025-12-11 12:04:09 -0700

    chore: add flake.nix and .envrc for Nix development

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "OpenCode plugin that automatically loads direnv environment variables";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nodejs_22
+              nodePackages.typescript
+            ];
+          };
+        });
+    };
+}

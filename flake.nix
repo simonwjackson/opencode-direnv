f257fa7f7e19ea8722fdfe546c2cb8b736d9387d
Simon W. Jackson <github@simonwjackson.io>
2025-12-11 13:43:59 -0700

    chore: update env var and demo configuration

--- flake.nix
+++ flake.nix
@@ -5,23 +5,23 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      devShells = forAllSystems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              nodejs_22
-              nodePackages.typescript
-            ];
-          };
-        });
-    };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_22
+          nodePackages.typescript
+          just
+        ];
+      };
+    });
+  };
 }
